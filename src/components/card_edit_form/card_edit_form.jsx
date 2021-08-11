e4cecf7e74a259b3d9741fb96f01beb6d9a7f6d9
naujkim <juno0218@naver.com>
2021-08-11 08:15:32 -0400

    add ImageUpload component

--- src/components/card_edit_form/card_edit_form.jsx
+++ src/components/card_edit_form/card_edit_form.jsx
@@ -1,9 +1,8 @@
 import React, { useRef } from 'react';
 import Button from '../button/button';
-import ImageFileInput from '../image_file_input/image_file_input';
 import styles from './card_edit_form.module.css';
 
-const CardEditForm = ({card, updateCard, deleteCard}) => {
+const CardEditForm = ({FileInput, card, updateCard, deleteCard}) => {
 
     const nameRef = useRef()
     const companyRef = useRef()
@@ -44,7 +43,7 @@ const CardEditForm = ({card, updateCard, deleteCard}) => {
             <input onChange={onChange} ref={emailRef} className={styles.input} type="text" name="email" value={email} />
             <textarea onChange={onChange} ref={messageRef} className={styles.textarea}  name="message" value={message}/>
             <div  className={styles.fileInput}>
-                <ImageFileInput/>
+                <FileInput/>
             </div>
             <Button name="Delete" onClick={onSubmit}/>
         </form>
--- src/components/maker/maker.jsx
+++ src/components/maker/maker.jsx
@@ -8,7 +8,7 @@ import styles from './maker.module.css';
 
 
 
-const Maker = ({authService}) => {
+const Maker = ({FileInput, authService}) => {
 
     const [cards, setCards] = useState({
         '1': {
@@ -84,7 +84,7 @@ const Maker = ({authService}) => {
         <section className={styles.maker}>
             <Header onLogout={onLogout}/>
             <div className={styles.container}>
-                <Editor cards={cards} addCard={createOrUpdateCard} updateCard={createOrUpdateCard} deleteCard={deleteCard} />
+                <Editor FileInput={FileInput} cards={cards} addCard={createOrUpdateCard} updateCard={createOrUpdateCard} deleteCard={deleteCard} />
                 <Preview cards={cards}/>
             </div>
             <Footer/>
